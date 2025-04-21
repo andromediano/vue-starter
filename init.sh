55d900260765e62a7547875031054b8bc1fdf6c5
andromediano <andromediano@gmail.com>
2025-04-22 06:34:34 +0900

    feat: Update init script

--- init.sh
+++ init.sh
@@ -2,40 +2,32 @@
 
 source $HOME/.config/colors.sh
 
-export PROJECT_CODE="vue-starter"
-export POSTGRES_DATA_PATH="${HOME}/development/podman/${PROJECT_CODE}/postgresql/data"
-SECRET_FILE="secret.yaml"
+# 스크립트 파일이 위치한 디렉토리의 전체 경로 구하기
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
 
+# 디렉토리명만 추출
+PROJECT_NAME="${SCRIPT_DIR:t}"
+
+export POSTGRES_DATA_PATH="${HOME}/development/podman/${PROJECT_NAME}/postgresql/data"
+SECRET_FILE="$SCRIPT_DIR/secret.yaml"
+
+#
 # pod.yaml 생성
-envsubst < pod.template.yaml > pod.yaml
+#
+envsubst < $SCRIPT_DIR/pod.template.yaml > $SCRIPT_DIR/pod.yaml
 
+#
 # Secret 파일 확인
+#
 if [ ! -f "$SECRET_FILE" ]; then
   echo "${RC_33}Error: Secret file $SECRET_FILE not found.${CR}"
   exit 1
 fi
 
-# # Secret에서 postgres-data-path 추출
-# POSTGRES_DATA_PATH=$(podman secret inspect my-secret --format '{{.Spec.Data.postgres-data-path}}' 2>/dev/null)
-# if [ -z "$POSTGRES_DATA_PATH" ]; then
-#   echo "Error: Failed to retrieve postgres-data-path from secret."
-#   exit 1
-# fi
-
-# # 템플릿 파일 확인
-# TEMPLATE_FILE="pod.template.yaml"
-# OUTPUT_FILE="pod.yaml"
-# if [ ! -f "$TEMPLATE_FILE" ]; then
-#   echo "Error: Template file $TEMPLATE_FILE not found."
-#   exit 1
-# fi
-
+#
 # 데이터 디렉토리 생성
+#
 mkdir -p "$POSTGRES_DATA_PATH"
-
-# # YAML 생성
-# sed -e "s|__POSTGRES_DATA_PATH__|$POSTGRES_DATA_PATH|g" \
-#     "$TEMPLATE_FILE" > "$OUTPUT_FILE"
 
 # # Secret 및 Pod 적용
 # podman play kube secret.yaml
